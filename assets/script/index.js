683d84ae241e92d4a29ffea6546b5fedce8f91c9
Deynni Almazan <deynnialmazan@hotmail.com>
2023-02-28 12:11:06 -0600

    Adding button Play again

--- assets/script/index.js
+++ assets/script/index.js
@@ -4,16 +4,16 @@ const number = document.querySelector('.number')
 const guessBtn = document.querySelector('.guess')
 const output = document.querySelector('.output')
 const total_guesses = document.querySelector('.total_guesses')
+const playAgain = document.querySelector('.play_again')
 
 function guessMyNumber (input) {
-    
     if (input > myNumber) {
         return 'My number is smaller';
     } else if (input < myNumber) {
         return 'My number is bigger';
     } return 'You guessed. Congratulations!';
-
 };
+
 
 let myNumber = Math.floor((Math.random() * 50) + 1);
 let times = 0;
@@ -22,7 +22,16 @@ guessBtn.addEventListener('click', function() {
     let a = number.value.trim(); 
     output.innerText = guessMyNumber(a);
     times = times + 1;
-    total_guesses.innerText = times;
+    total_guesses.innerText = 'Guesses: ' + `${times}`;
 }
+);
 
+
+
+playAgain.addEventListener('click', function() {
+    times = 0; 
+    number.value = "";
+    output.innerText = "";
+    total_guesses.innerText = "";;
+}
 );
