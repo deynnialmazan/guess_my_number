b4eb1b1e6cd91a676acb552468322d557a8b8782
Deynni Almazan <deynnialmazan@hotmail.com>
2023-02-28 11:39:45 -0600

    Function added to JS

--- assets/script/index.js
+++ assets/script/index.js
@@ -3,22 +3,26 @@
 const number = document.querySelector('.number')
 const guessBtn = document.querySelector('.guess')
 const output = document.querySelector('.output')
+const total_guesses = document.querySelector('.total_guesses')
 
 function guessMyNumber (input) {
-    let myNumber = Math.floor((Math.random() * 50) + 1);
+    
     if (input > myNumber) {
         return 'My number is smaller';
     } else if (input < myNumber) {
         return 'My number is bigger';
-    } return 'You win. Congratulations!';
+    } return 'You guessed. Congratulations!';
 
 };
 
+let myNumber = Math.floor((Math.random() * 50) + 1);
+let times = 0;
+
 guessBtn.addEventListener('click', function() {
     let a = number.value.trim(); 
-    guessMyNumber(a);
     output.innerText = guessMyNumber(a);
-
-
+    times = times + 1;
+    total_guesses.innerText = times;
 }
+
 );
